8c9817663c24170db8450eeb67bff7bca84822ad
kamilsmr <kamils6035@gmail.com>
2021-12-22 00:37:49 +0300

    asenkron

---
+++ javaScript/callback.js
@@ -0,0 +1,46 @@
+// console.log("1.görev");
+// console.log("2.görev");
+
+// console.log("3.görev");
+
+// const func1 = () => {
+//   console.log("Func 1 tamamlandı");
+// };
+
+// const func2 = () => {
+//   console.log("Func 2 tamamlandı");
+// };
+
+// const func1 = () => {
+//   console.log("Func 1 tamamlandı");
+//   func2();
+// };
+
+// const func2 = () => {
+//   console.log("Func 2 tamamlandı");
+//   func3();
+// };
+
+// const func3 = () => {
+//   console.log("Func 3 tamamlandı");
+// };
+
+// func1();
+
+// const func1 = () => {
+//   console.log("Func 1 tamamlandı");
+//   func3();
+// };
+
+// const func2 = () => {
+//   console.log("Func 2 tamamlandı");
+// };
+
+// const func3 = () => {
+//   console.log("Func 3 tamamlandı");
+//   func1(2);
+// };
+
+// func1();
+
+//1-3-2  javascript herzaman senkron çalışır. single treat çalışır. manipule ederk asenkron çalışmasını sağlarız
