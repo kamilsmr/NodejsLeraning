e045350662a7967d184edbebc6ec53c54b202019
kamilsmr <kamils6035@gmail.com>
2021-12-22 01:16:50 +0300

    add

--- javaScript/callback.js
+++ javaScript/callback.js
@@ -44,3 +44,44 @@
 // func1();
 
 //1-3-2  javascript herzaman senkron çalışır. single treat çalışır. manipule ederk asenkron çalışmasını sağlarız
+
+// let x = 5;
+// console.log("1.gelen veri: ", x);
+
+// setTimeout(() => {
+//   x = x + 5;
+// }, 5000);
+// console.log("2.gelen veri: ", x);
+
+// x = x + 5;
+// console.log("3.gelen veri: ", x);
+
+// let x = 5;
+// console.log("1.gelen veri: ", x);
+
+// setTimeout(() => {
+//   x = x + 5;
+//   console.log("2.gelen veri: ", x);
+// }, 5000);
+
+// x = x + 5;
+// console.log("3.gelen veri: ", x);
+
+// const books = [
+//   { name: "Kitap 1", author: "Yazar 1" },
+//   { name: "Kitap 2", author: "Yazar 2" },
+//   { name: "Kitap 3", author: "Yazar 3" },
+// ];
+
+// const listBooks = () => {
+//   books.map((book) => {
+//     console.log(book.name);
+//   });
+// };
+
+// const addBook = (newBook) => {
+//   books.push(newBook);
+// };
+
+// addBook({ name: "Kitap 4", author: "Yazar 4" });
+// listBooks();
